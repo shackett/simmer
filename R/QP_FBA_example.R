51fe41f5029bcdad59c6cfbf052570056a7f4eab
shackett <shackett@princeton.edu>
2016-09-09 12:22:15 -0400

    old changes

--- R/QP_FBA_example.R
+++ R/QP_FBA_example.R
@@ -21,8 +21,11 @@ format_boundary_conditions()
 # identify reaction that cannot carry flux based on inputs, outputs and directionality
 infRxMet <- setup_FBA_constraints(return_infeasible = T, infRxMet = NULL)
 
+# prune infeasible reactions and format data for optimization
 setup_FBA_constraints(return_infeasible = F, infRxMet)
 
+# calculate fluxes using quadratic programming
 inferred_fluxes <- calculate_QP_fluxes()
 
+# generate files to read into python for calculating upper and lower bounds on flux using FVA
 read_write_python_FVA(status = "write", directory = "~/Desktop/")
